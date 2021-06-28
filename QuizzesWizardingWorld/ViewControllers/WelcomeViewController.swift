2c19ba5f7a6cdab5e38e1089538a9dd1cf7c34aa
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-28 19:18:03 +0300

    Create home screen

---
+++ QuizzesWizardingWorld/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  QuizzesWizardingWorld
+//
+//  Created by Sergey Lukaschuk on 28.06.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.barStyle = .black
+    }
+
+
+}
+
