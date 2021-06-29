2ca5cbe8b2440eeedab2f407ac49fdd098106763
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-29 08:58:17 +0300

    Add 'QuizzesViewController'

---
+++ QuizzesWizardingWorld/ViewControllers/QuizzesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  QuizzesViewController.swift
+//  QuizzesViewController
+//
+//  Created by Sergey Lukaschuk on 29.06.2021.
+//
+
+import UIKit
+
+class QuizzesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
